7d2bb4b20569b595ba5728e5b05dffbca1f19eb6
Eric Joo <ericjaeminjoo@gmail.com>
2018-07-10 17:41:07 +0000

    Create react components

---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Message extends Component {
+  render() {
+    return (
+      <div className="message">
+        <span className="message-username">Anonymous1</span>
+        <span className="message-content">
+          In blandit, ex at congue convallis, ante mauris placerat nunc, nec
+          dictum lectus dolor sit amet elit.
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Message;

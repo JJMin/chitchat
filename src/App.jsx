f492c970b97232cff59fa2f1d05762266e18b5b1
Eric Joo <ericjaeminjoo@gmail.com>
2018-07-11 17:02:25 +0000

    Setup/connect WebSockets and send messages to the server

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,16 @@ class App extends Component {
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages })
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
+    //   const messages = this.state.messages.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({ messages: messages })
+    // }, 3000);
+    this.chitchatWebSocket = new WebSocket("ws://localhost:3001/");
   }
 
   newMessageHandler(e) {
@@ -44,6 +45,10 @@ class App extends Component {
         username: this.state.currentUser.name,
         content: e.target.value
       };
+
+      // Send the msg object as a JSON-formatted string
+      this.chitchatWebSocket.send(JSON.stringify(newMessageObj));
+
       const newMessages = [...oldMessages, newMessageObj];
       this.setState({
         messages: newMessages
